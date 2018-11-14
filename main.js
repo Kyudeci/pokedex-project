24ec03e155f191cd3294fff0d39a90bef863927f
Shakiem Johnson <sjohnson@nyc.yearup.org>
2018-11-14 12:25:57 -0500

    Prepping JS

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ let ball = document.getElementById("pbody")
 
 function shake(){
   ball.classList.add("shake")
+  document.getElementById("return").style.visibility = "visible"
 }
 
-button.addEventListener("click", shake)
+button.addEventListener("click", shake);
+
+
+class Pokemon {
+  constructor(name) {
+    this.name = name
+    this.hp =
+    this.attack =
+    this.defense =
+    this.abilities = abilities
+  }
+}
