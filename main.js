3c534c5bbbd24c556b4538167cbd81971e05926d
Shakiem Johnson <sjohnson@nyc.yearup.org>
2018-11-15 14:48:03 -0500

    unown adjustments

--- main.js
+++ main.js
@@ -9,14 +9,30 @@ function shake(){
 }
 
 ball.addEventListener("click", shake);
-
-
-class Pokemon {
-  constructor(name) {
-    this.name = name
-    this.hp = hp
-    this.attack = attack
-    this.defense = defense
-    this.abilities = abilities
-  }
+// Make the call
+function display() {
+  let x = 5
+  axios.get("http://fizal.me/pokeapi/api/v2/id/"+ x +".json")
+  .then(function (response) {
+  console.log(response);
+})
+.catch(function (error) {
+  console.log(error);
+})
+.then(function () {
+});
 }
+
+// response.data.
+//
+// class Pokemon {
+//   constructor(name) {
+//
+//     this.name = name
+//     this.hp = hp
+//     this.attack = attack
+//     this.defense = defense
+//     this.abilities = abilities
+//     this.sprite = sprite
+//   }
+// }
