868f93122df5026da8ad2fc00b860c7dca50b0c9
Shakiem Johnson <sjohnson@nyc.yearup.org>
2018-11-15 16:55:01 -0500

    A bunch of things

--- main.js
+++ main.js
@@ -9,30 +9,45 @@ function shake(){
 }
 
 ball.addEventListener("click", shake);
-// Make the call
-function display() {
-  let x = 5
-  axios.get("http://fizal.me/pokeapi/api/v2/id/"+ x +".json")
-  .then(function (response) {
-  console.log(response);
-})
-.catch(function (error) {
-  console.log(error);
-})
-.then(function () {
-});
-}
+
+// // Make the call
+// function display() {
+//   let x = 5
+//   axios.get("http://fizal.me/pokeapi/api/v2/id/"+ x +".json")
+//   .then(function (response) {
+//   console.log(response);
+// })
+// .catch(function (error) {
+//   console.log(error);
+// })
+// .then(function () {
+// });
+// }
 
 // response.data.
-//
-// class Pokemon {
-//   constructor(name) {
-//
-//     this.name = name
-//     this.hp = hp
-//     this.attack = attack
-//     this.defense = defense
-//     this.abilities = abilities
-//     this.sprite = sprite
-//   }
-// }
+
+class Pokemon {
+  constructor(name) {
+    this.name = name;
+    this.id = id;
+    this.hp = hp;
+    this.attack = attack;
+    this.defense = defense;
+    this.abilities = abilities;
+    this.sprite = sprite;
+  }
+    if (response.data.id > 721) {
+      "https://www.pkparaiso.com/imagenes/sol-luna/sprites/animados/" + response.data.id + ".gif"
+    }
+}
+class Trainer {
+  constructor() {
+    this.pokemon = []
+  }
+  all() {
+
+  }
+  get(name){
+
+  }
+}
