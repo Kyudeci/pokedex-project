c94a94df5ab47d17feb3ea83c56d23eef05cfd8f
Kyu <kyudeci@gmail.com>
2018-11-24 17:49:09 -0500

    Mega Evolutions, etc. added; Pokemon cries; Button Icons

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
-let button = document.getElementById("random")
-let ball = document.getElementById("new")
-let tripleball = document.getElementById("display")
-let box1 = document.getElementById("box")
-let zoro = document.getElementById('adjust')
+// Background
+let ovr = document.querySelector("html");
+// Buttons
+let button = document.getElementById("random");
+let mega = document.getElementById("form");
+let megaX = document.getElementById("shinkaX");
+let megaY = document.getElementById("shinkaY");
+let revert = document.getElementById("revert");
+let orig = document.getElementById("return");
+let necroz = document.getElementById("necrozma");
+// Search
 let input = document.querySelector('input');
-let ovr = document.querySelector("html")
-let submit = document.getElementById("submit")
-let basic1 = document.getElementById("pkmn1")
-let basic2 = document.getElementById("pkmn2")
-let basic3 = document.getElementById("pkmn3")
+let submit = document.getElementById("submit");
+// Information display
+let box1 = document.getElementById("box");
+let zoro = document.getElementById('adjust');
+// Poké Balls
+let ball = document.getElementById("new");
+let tripleball = document.getElementById("display");
+let basic1 = document.getElementById("pkmn1");
+let basic2 = document.getElementById("pkmn2");
+let basic3 = document.getElementById("pkmn3");
 
 function shake(){
   // ball.classList.add("shake")
@@ -36,21 +47,6 @@ function nanib() {
 function nanic() {
   basic3.src = "Beastball.png";
 }
-function xyz() {
-  if (input.value == "solgaleo") {
-    box1.style.background = "url('sol2.png') no-repeat center center";
-    ovr.style.background = "#033e50 url(alola.jpg) no-repeat center center fixed";
-  } else if (input.value == "lunala") {
-    box1.style.background = "url('sol2.png') no-repeat center center";
-    ovr.style.background = "#033e50 url(alola.jpg) no-repeat center center fixed";
-  } else if (input.value == "pikachu") {
-    ovr.style.background = "#ffd624 url(pikaw.png) no-repeat center center fixed";
-    zoro.src = "mimikyu.gif";
-  } else {
-    box1.style.background = "white";
-  }
-}
-
 // Events
 ball.addEventListener("click", shake);
 button.addEventListener("click", display);
@@ -64,9 +60,7 @@ basic3.addEventListener("click", kartana);
 basic3.addEventListener("mouseover", anic);
 basic3.addEventListener("mouseout", nanic);
 submit.addEventListener("click", search);
-submit.addEventListener("click", xyz);
-
-// // Make the call
+// Make the call
 function display() {
   let x = Math.floor(Math.random() * 802) + 1
   axios.get("https://fizal.me/pokeapi/api/v2/id/"+ x +".json")
@@ -199,21 +193,106 @@ class Pokemon {
   sprites() {
     let src = document.getElementById("adjust");
     let box = document.getElementById("box");
-    src.id = "adjust"
-    box.appendChild(src)
-    let spr = this.sprite
-    let fix = spr.replace('-','')
-    //Appear the sprite that leads to the future.
-    if (this.id == 25) {
-      src.src == "https://play.pokemonshowdown.com/sprites/xyani/mimikyu-totem.gif"
-    } else {
-      src.src = "https://play.pokemonshowdown.com/sprites/xyani/"+ fix +".gif"
+    src.id = "adjust";
+    box.appendChild(src);
+    let spr = this.sprite;
+    let fix = spr.replace('-','');
+//Appear the sprite that leads to the future.
+    src.src = "https://play.pokemonshowdown.com/sprites/xyani/"+ fix +".gif";
+    function xyz() {
+      if (spr == "solgaleo" || spr == "lunala") {
+        box1.style.background = "url('sol2.png') no-repeat center center";
+        ovr.style.background = "#033e50 url(alola.jpg) no-repeat center center fixed";
+      } else {
+        box1.style.background = "white";
+      }
     }
+    submit.addEventListener("click", xyz);
+//Pokemon cries and then some
     let audino = document.getElementById("cry");
     audino.autoplay = true;
     let body = document.querySelector("body");
     let cry = "https://play.pokemonshowdown.com/audio/cries/"+ fix +".mp3"
     audino.src = cry
+    if (this.id == 109) {
+      audino.src = "http://play.pokemonshowdown.com/audio/bw2-homika-dogars.mp3"
+    } else if (this.id == 272) {
+      audino.src = "http://play.pokemonshowdown.com/audio/colosseum-miror-b.mp3"
+    } else if (spr == "meowth") {
+      audino.src = "TeamR.mp3";
+    }
+//Mega Evolution--visual changes only
+    let megas = ["venusaur", "blastoise", "alakazam", "gengar", "kangaskhan", "pinsir", "gyarados", "aerodactyl", "ampharos", "scizor", "heracross", "houndoom", "tyranitar", "blaziken", "gardevoir", "mawile", "aggron", "medicham", "banette", "absol", "garchomp", "lucario", "abomasnow", "beedrill", "pidgeot", "slowbro", "steelix", "sceptile", "swampert", "sableye", "sharpedo", "camerupt", "altaria", "glalie", "salamence", "metagross", "latias", "latios", "rayquaza", "lopunny", "gallade", "audino", "diancie"]
+    let megas2 = ["charizard", "mewtwo"]
+    let primal = ["kyogre", "groudon"]
+    if (megas.includes(spr)) {
+      mega.style.visibility = "visible";
+    } else {
+      mega.style.visibility = "hidden";
+    }
+    if (megas2.includes(spr)) {
+      megaX.style.visibility = "visible";
+      megaY.style.visibility = "visible";
+    } else {
+      megaX.style.visibility = "hidden";
+      megaY.style.visibility = "hidden";
+    }
+    if (primal.includes(spr)) {
+      revert.style.visibility = "visible";
+    } else {
+      revert.style.visibility = "hidden";
+    }
+    if (megas.includes(spr) || megas2.includes(spr) || primal.includes(spr)) {
+      orig.style.visibility = "visible";
+    } else {
+      orig.style.visibility = "hidden";
+    }
+    if (spr == "necrozma") {
+      necroz.style.visibility = "visible";
+    } else {
+      necroz.style.visibility = "hidden";
+    }
+    function megaEvolve() {
+      if (megas.includes(spr)) {
+        src.src = "https://play.pokemonshowdown.com/sprites/xyani/"+ spr +"-mega.gif"
+        audino.src = "https://play.pokemonshowdown.com/audio/cries/"+ spr +"-mega.mp3"
+      }
+    }
+    function megaEvolveX() {
+      if (megas2.includes(spr)) {
+        src.src = "https://play.pokemonshowdown.com/sprites/xyani/"+ spr +"-megax.gif"
+        audino.src = "https://play.pokemonshowdown.com/audio/cries/"+ spr +"-megax.mp3"
+      }
+    }
+    function megaEvolveY() {
+      if (megas2.includes(spr)) {
+        src.src = "https://play.pokemonshowdown.com/sprites/xyani/"+ spr +"-megay.gif"
+        audino.src = "https://play.pokemonshowdown.com/audio/cries/"+ spr +"-megay.mp3"
+      }
+    }
+    function primalR() {
+      if (primal.includes(spr)) {
+        src.src = "https://play.pokemonshowdown.com/sprites/xyani/"+ spr +"-primal.gif"
+        audino.src = "https://play.pokemonshowdown.com/audio/cries/"+ spr +"-primal.mp3"
+      }
+    }
+    function original() {
+      if (megas.includes(spr) || megas2.includes(spr) || primal.includes(spr)) {
+        src.src = "https://play.pokemonshowdown.com/sprites/xyani/"+ fix +".gif";
+      }
+    }
+    function necrozma() {
+      if (spr == "necrozma") {
+        src.src = "https://play.pokemonshowdown.com/sprites/xyani/necrozma-ultra.gif";
+        audino.src = "https://themeplaza.eu/download/3226/bgm";
+      }
+    }
+    mega.addEventListener("click", megaEvolve)
+    megaX.addEventListener("click", megaEvolveX)
+    megaY.addEventListener("click", megaEvolveY)
+    revert.addEventListener("click", primalR)
+    orig.addEventListener("click", original)
+    necroz.addEventListener("click", necrozma)
   }
   stats() {
     let li = document.createElement("li");
